1d26a88d94d31cfd3653150414583495da9c8ecf
B V VYNAVI <94527237+vynavi@users.noreply.github.com>
2024-11-27 06:34:36 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Dark Mode Toggle
+const themeToggleButton = document.getElementById("theme-toggle");
+
+themeToggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
